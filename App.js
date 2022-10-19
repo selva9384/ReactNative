71c539198172aac847c7deffb61b90910ed27ee9
selva9384 <selvendiran938496@gmail.com>
2022-10-19 07:14:13 +0530

    code magic configuration

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+const App=()=>{
+  return(
+    <View style={{flex:1,backgroundColor:'red',justifyContent:'center',alignItem:'center'}}>
+      <Text style={{backgroundColor:"blue",textAlign:'center'}}>hello world</Text>
+    </View>);
+}
+export default App;
